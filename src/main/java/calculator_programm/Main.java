ce413c2e0863241f9c4d78d741a6779eff3582ab
VladimirJG <vladimi85@gmail.com>
2023-04-21 10:31:04 +0300

    modified

--- src/main/java/calculator_programm/Main.java
+++ src/main/java/calculator_programm/Main.java
@@ -4,26 +4,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        //String newOperation = reader.readLine();
-        String str = "10 - 5";
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < str.length(); i++) {
-            list.add(String.valueOf(str.charAt(i)));
-        }
-        System.out.println(list);
+       /* String newOperation = reader.readLine();
+        System.out.println(gettingData(newOperation));*/
 
     }
 
-    public static int gettingNumbers(String str) {
+    public static List<String> gettingData(String string) {
+        String[] data = string.split(" ");
+        List<String> listOfData = new ArrayList<>();
+        if (data.length == 3) {
+            if ((Integer.parseInt(data[0]) > 0 && Integer.parseInt(data[0]) < 11) && (Integer.parseInt(data[2]) > 0 && Integer.parseInt(data[2]) < 11)) {
+                listOfData.add(data[0]);
+                listOfData.add(data[2]);
+                listOfData.add(data[1]);
+            }
+            return listOfData;
+        } else if (data.length < 3) {
+            System.out.println("throws Exception //т.к. строка не является математической операцией");
+        } else {
+            System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+        return null;
+    }
+
+    public static boolean arabNumbersGetting(String str) {
         int[] arabNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < arabNumbers.length; i++) {
+            if (Integer.parseInt(str) == arabNumbers[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean romanNumbersGetting(String str) {
         String[] romanNumbers = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        return 0;
+        for (int i = 0; i < romanNumbers.length; i++) {
+            if (str.equals(romanNumbers[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void operandSelection(String str) {
--- src/main/java/calculator_programm/Test.java
+++ src/main/java/calculator_programm/Test.java
@@ -1,4 +1,29 @@
 package calculator_programm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Test {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] newOperation = reader.readLine().split(" ");
+
+        if (newOperation.length == 2) {
+            String firstOperand = newOperation[0];
+            String secondOperand = newOperation[2];
+            String operator = newOperation[1];
+
+        } else {
+
+            System.out.println("Формат введенных значений не соответствует условиям. Пример: 1 + 1\n Повторите ввод");
+        }
+
+
+    }
+
 }
+
