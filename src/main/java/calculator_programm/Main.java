b018127780a7df313280284a5b922fa8bacc21fd
VladimirJG <vladimi85@gmail.com>
2023-04-21 13:16:10 +0300

    Added method

--- src/main/java/calculator_programm/Main.java
+++ src/main/java/calculator_programm/Main.java
@@ -13,6 +13,12 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
        /* String newOperation = reader.readLine();
         System.out.println(gettingData(newOperation));*/
+        List<String> list = new ArrayList<>();
+        list.add("0");
+        list.add("10");
+        list.add("-");
+        System.out.println(typeChecking(list));
+        // System.out.println(romanToArab("II"));
 
     }
 
@@ -64,6 +70,26 @@ public class Main {
         return false;
     }
 
+    public static List<Integer> typeChecking(List<String> listInputData) {
+        List<Integer> outputDataList = new ArrayList<>();
+        if (checkingTheEnteredValueForMinimumAndMaximum(listInputData.get(0)) && checkingTheEnteredValueForMinimumAndMaximum(listInputData.get(1))) {
+            outputDataList.add(0, Integer.parseInt(listInputData.get(0)));
+            outputDataList.add(1, Integer.parseInt(listInputData.get(1)));
+            return outputDataList;
+        } else if (romanNumbersGetting(listInputData.get(0)) && romanNumbersGetting(listInputData.get(1))) {
+            outputDataList.add(0, romanToArab(listInputData.get(0)));
+            outputDataList.add(1, romanToArab(listInputData.get(1)));
+            return outputDataList;
+        } else {
+            System.out.println("throws Exception //т.к. используются одновременно разные системы счисления");
+        }
+        return null;
+    }
+
+    public static void operation(String inputOperation) {
+        List<String> listOfData = gettingData(inputOperation);
+    }
+
     public static void operandSelection(String str) {
         char[] array = str.toCharArray();
         for (int i = 0; i < array.length; i++) {
@@ -73,22 +99,28 @@ public class Main {
         }
     }
 
+    public static boolean checkingTheEnteredValueForMinimumAndMaximum(String string) {
+        if (!romanNumbersGetting(string) && arabNumbersGetting(string) && Integer.parseInt(string) > 0 && Integer.parseInt(string) < 11) {
+            return true;
+        } else {
+            System.out.println("Введенное значение не соответствует условиям");
+        }
+        return false;
+    }
+
     public static int plus(int a, int b) {
         return a + b;
     }
 
     public static int minus(int a, int b) {
-        //напишите тут ваш код
         return a - b;
     }
 
     public static int multiplication(int a, int b) {
-        //напишите тут ваш код
         return a * b;
     }
 
     public static int division(int a, int b) {
-        //напишите тут ваш код
         return a / b;
     }
 }
